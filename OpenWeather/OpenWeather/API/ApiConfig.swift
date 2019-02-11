8c30413e28b73e50daacd0487d613608030f0582
rodrigo-placido <rodrigo.placido@gmail.com>
2019-02-11 10:03:28 -0200

    add alamofire and create apiconfig

---
+++ OpenWeather/OpenWeather/API/ApiConfig.swift
@@ -0,0 +1,18 @@
+//
+//  ApiConfig.swift
+//  OpenWeather
+//
+//  Created by Rodrigo Goncalez on 11/02/19.
+//  Copyright © 2019 Rodrigo Goncalez. All rights reserved.
+//
+
+import Foundation
+
+struct ApiConfig {
+    static let apiHost = "http://api.openweathermap.org/data/2.5/"
+    static let apiKey = "7d01c7a2d8be734b04a8397cbd8c4356"
+    
+    static func getUrlFindByCoordinates(lat: String, lon: String) -> String {
+        return "\(apiHost)find?lat=\(lat)&lon=\(lon)"
+    }
+}
---
+++ OpenWeather/OpenWeather/Services/GetCurrentWeatherService.swift
@@ -0,0 +1,12 @@
+//
+//  GetCurrentWeatherService.swift
+//  OpenWeather
+//
+//  Created by Rodrigo Goncalez on 11/02/19.
+//  Copyright © 2019 Rodrigo Goncalez. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
