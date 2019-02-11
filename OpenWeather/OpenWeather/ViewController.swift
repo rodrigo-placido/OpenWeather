59495fef7775fa62cfa48006622970a04bca1291
rodrigo-placido <rodrigo.placido@gmail.com>
2019-02-11 08:24:29 -0200

    add project and config cocoapods

---
+++ OpenWeather/OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Rodrigo Goncalez on 11/02/19.
+//  Copyright © 2019 Rodrigo Goncalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
